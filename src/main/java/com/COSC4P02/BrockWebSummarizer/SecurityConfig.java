0d64eefb3e9404649f557d5f8f2f46ed39ff4146
Ralph Terte <109493278+Jaserii@users.noreply.github.com>
2024-02-02 12:57:27 -0500

    Added test "pro" site

---
+++ src/main/java/com/COSC4P02/BrockWebSummarizer/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.COSC4P02.BrockWebSummarizer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/config/annotation/web/builders/HttpSecurity.html#oauth2Login(org.springframework.security.config.Customizer)
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/pro").authenticated();
+                    auth.anyRequest().permitAll();
+                })
+                .oauth2Login(withDefaults())
+                .build();
+    }
+}
--- src/main/java/com/COSC4P02/BrockWebSummarizer/WebController.java
+++ src/main/java/com/COSC4P02/BrockWebSummarizer/WebController.java
@@ -1,16 +1,28 @@
 package com.COSC4P02.BrockWebSummarizer;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
+import java.util.Map;
+
 @Controller
 public class WebController {
 
-    @GetMapping
-    String test(Model model) {
-        model.addAttribute("test", "Data from WebController"); // data to send to html page
+    @GetMapping("/")
+    String index(Model model) {
+        model.addAttribute("index", "Data from WebController"); // data to send to html page
         return "index"; // webpage name
     }
 
+    @GetMapping("/pro")
+    String pro(Model model, @AuthenticationPrincipal OAuth2User principal) {
+        System.out.println(principal.getAttributes());
+
+        // Return pro site with additional features
+        model.addAttribute("pro", "Data from WebController");
+        return "pro";
+    }
 }
