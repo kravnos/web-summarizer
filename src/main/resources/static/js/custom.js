6f86651bd698c9b7e46585fb45db03d882368b28
tv15jl <tv15jl@brocku.ca>
2024-02-05 20:31:02 -0500

    darkmode sessionbased

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    var bDark = sessionStorage.getItem('bDark') || '';
+
+    if (bDark == 'true') {
+        $("body, .modal-content").addClass("bg-secondary");
+        $("h1, h2, h3, h4, h5, p").addClass("text-white");
+        $("nav").addClass("bg-dark");
+        $("button").addClass("btn-dark");
+        $(".dkmode-check-label").toggleClass("bi-moon-fill");
+        $(".dkmode-check-label").toggleClass("bi-brightness-high-fill");
+        $("#flexSwitchCheckDefault").each(function() { this.checked = true; });
+    }
+
     $("#flexSwitchCheckDefault").change(function() {
         $("body, .modal-content").toggleClass("bg-secondary");
         $("h1, h2, h3, h4, h5, p").toggleClass("text-white");
@@ -6,5 +18,11 @@ $(document).ready(function () {
         $("button").toggleClass("btn-dark");
         $(".dkmode-check-label").toggleClass("bi-moon-fill");
         $(".dkmode-check-label").toggleClass("bi-brightness-high-fill");
+
+        if ($("#flexSwitchCheckDefault").is(':checked')) {
+            sessionStorage.setItem('bDark','true');
+        } else {
+            sessionStorage.removeItem('bDark');
+        }
     });
 });
