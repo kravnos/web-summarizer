379978646e12e76a1b0b2298bfefa1abd5e355fa
tv15jl <tv15jl@brocku.ca>
2024-02-09 14:28:30 -0500

    server response delay testing

--- src/main/java/com/websummarizer/Web/Summarizer/controller/WebController.java
+++ src/main/java/com/websummarizer/Web/Summarizer/controller/WebController.java
@@ -1,16 +1,21 @@
 package com.websummarizer.Web.Summarizer.controller;
 
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
 
-    @GetMapping("/api/summary")
+    @SneakyThrows
+    @PostMapping("/api/summary")
     String getSummary(Model model) {
         model.addAttribute("summaryText", "<< Data from WebController >>"); // data to send to html page
+        //Thread.sleep(500); // testing server response delays - delete later
         return "api/summary"; // webpage to return
     }
 
