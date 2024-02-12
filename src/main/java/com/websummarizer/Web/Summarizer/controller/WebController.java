b56b859972555d1142a410e91511394e7c2c5c83
tv15jl <tv15jl@brocku.ca>
2024-02-12 12:37:26 -0500

    data update

--- src/main/java/com/websummarizer/Web/Summarizer/controller/WebController.java
+++ src/main/java/com/websummarizer/Web/Summarizer/controller/WebController.java
@@ -11,11 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-    @SneakyThrows
     @PostMapping("/api/summary")
     String getSummary(Model model) {
-        model.addAttribute("summaryText", "<< Example Data from WebController >> The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog<br /><br />The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog << END >>"); // data to send to html page
-        return "api/summary"; // webpage to return
+        model.addAttribute("summaryText", "<< Example Data from WebController >> The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog<br /><br />The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog << END >>");
+        return "api/summary";
     }
 
 }
