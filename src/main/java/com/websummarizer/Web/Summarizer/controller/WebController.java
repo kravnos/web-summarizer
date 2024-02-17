1f0648703f3ec0b1476fd8017009746651751dda
tv15jl <tv15jl@brocku.ca>
2024-02-17 17:14:36 -0500

    date, input, output added

--- src/main/java/com/websummarizer/Web/Summarizer/controller/WebController.java
+++ src/main/java/com/websummarizer/Web/Summarizer/controller/WebController.java
@@ -1,6 +1,5 @@
 package com.websummarizer.Web.Summarizer.controller;
 
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.Mapping;
@@ -8,12 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 @Controller
 public class WebController {
 
     @PostMapping("/api/summary")
     String getSummary(Model model) {
-        model.addAttribute("summaryText", "<< Example Data from WebController >> The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog<br /><br />The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog << END >>");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd h:mm:ss a");
+        Date date = new Date();
+
+        model.addAttribute("date", dateFormat.format(date));
+        model.addAttribute("input", "input text");
+        model.addAttribute("output", "<< Example Data from WebController >> The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog<br /><br />The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog << END >>");
         return "api/summary";
     }
 
