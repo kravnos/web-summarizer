af994b5e1e2e77ba341dd2dc114193c3eb103ab0
tv15jl <tv15jl@brocku.ca>
2024-02-18 22:29:13 -0500

    input added to controller

--- src/main/java/com/websummarizer/Web/Summarizer/controller/WebController.java
+++ src/main/java/com/websummarizer/Web/Summarizer/controller/WebController.java
@@ -2,10 +2,7 @@ package com.websummarizer.Web.Summarizer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.text.DateFormat;
@@ -15,13 +12,24 @@ import java.text.SimpleDateFormat;
 public class WebController {
 
     @PostMapping("/api/summary")
-    String getSummary(Model model) {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd h:mm:ss a");
+    public String getSummary(
+            @RequestParam(value = "input") String input,
+            Model model
+    ) {
         Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd h:mm:ss a");
+
+        String username = "You";
+        //if (bLoggedIn == true) {
+            // set username to logged in name
+        //}
+        //String input = "Input text or URL";
+        String output = "<< Example Data from WebController >> The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog<br /><br />The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog << END >>";
 
         model.addAttribute("date", dateFormat.format(date));
-        model.addAttribute("input", "input text");
-        model.addAttribute("output", "<< Example Data from WebController >> The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog<br /><br />The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog The quick brown fox jumped over the lazy dog << END >>");
+        model.addAttribute("user", username);
+        model.addAttribute("input", input);
+        model.addAttribute("output", output);
         return "api/summary";
     }
 
