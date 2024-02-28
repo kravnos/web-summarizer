0c65db06fa7e76173680d16915b1dda59189920a
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-02-28 17:26:54 -0500

    Improved code for BART class to avoid connection overhead

--- src/main/java/com/websummarizer/Web/Summarizer/bart/Bart.java
+++ src/main/java/com/websummarizer/Web/Summarizer/bart/Bart.java
@@ -4,7 +4,6 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -27,18 +26,10 @@ public class Bart {
         this.headers.setBearerAuth(AUTH_TOKEN);
     }
 
-    public String queryModelURL(@RequestParam String url){
-
-        //  Gets URL from browser URL, extract paragraph values, then put into requestBody
-        //  Example: localhost:8080/test?url=https://en.wikipedia.org/wiki/Brock_University  ==> can change this to other link to test
-
-        String prompt;
+    public String queryModel(String inputText) {
         try {
-            prompt = HTMLParser.parser(url);
-            String requestBody = "{\"inputs\": \"" + prompt + "\"}";
+            String requestBody = "{\"inputs\": \"" + inputText + "\"}";
             HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
-
-            headers.setBearerAuth(AUTH_TOKEN);
 
             responseEntity = restTemplate.exchange(
                     API_URL, HttpMethod.POST, requestEntity, String.class);
@@ -46,7 +37,6 @@ public class Bart {
             if (responseEntity.getStatusCode().is2xxSuccessful()) {
                 JSONArray jsonArray = new JSONArray(responseEntity.getBody());
                 JSONObject jsonObject = jsonArray.getJSONObject(0);
-
                 return jsonObject.getString("summary_text");
             } else {
                 return "Cannot summarize at the moment. Please try again later.";
@@ -61,29 +51,5 @@ public class Bart {
         }
     }
 
-    public String queryModelText(String inputText) {
-        try {
-            String requestBody = "{\"inputs\": \"" + inputText + "\"}";
-            HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
-
-            ResponseEntity<String> responseEntity = restTemplate.exchange(
-                    API_URL, HttpMethod.POST, requestEntity, String.class);
-
-            if (responseEntity.getStatusCode().is2xxSuccessful()) {
-                JSONArray jsonArray = new JSONArray(responseEntity.getBody());
-                JSONObject jsonObject = jsonArray.getJSONObject(0);
-                return jsonObject.getString("summary_text");
-            } else {
-                return "Cannot summarize at the moment. Please try again later.";
-            }
-        } catch (HttpClientErrorException e) {
-            // Log the error for debugging
-            System.out.println("HTTP client error");
-            return "Error: " + e.getRawStatusCode() + " " + e.getStatusText();
-        } catch (Exception e) {
-            // Handle other exceptions
-            return "An error occurred. Please try again later.";
-        }
-    }
 
 }
