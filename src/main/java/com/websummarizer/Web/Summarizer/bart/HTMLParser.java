8c1de581ebc6cad5dbf4835c51e71337ec908098
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-02-28 21:04:08 -0500

    Removed unnecessary code from HTML parser

--- src/main/java/com/websummarizer/Web/Summarizer/bart/HTMLParser.java
+++ src/main/java/com/websummarizer/Web/Summarizer/bart/HTMLParser.java
@@ -8,29 +8,16 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class HTMLParser {
+    public static String parser(String url) throws IOException {
+        // Get the URL, selects every paragraph element, extract the text value of each paragraph
+        Document doc = Jsoup.connect(url).get();
 
-    public static String parser(String url) {
-        try {
-            // Get the URL, selects every paragraph element, extract the text value of each paragraph
-            Document doc = (Document) Jsoup.connect(url).get();
-
-            Elements paragraphs = doc.select("p");
-            StringBuilder websiteData = new StringBuilder();
-            for (Element p : paragraphs) {
-                websiteData.append(p.text()).append(" ");
-            }
-            return websiteData.toString().trim();
-        } catch (IOException e) {
-            // Handle IOException (e.g., connection error)
-            return "Request Error: " + e.getMessage();
+        Elements paragraphs = doc.select("p");
+        StringBuilder websiteData = new StringBuilder();
+        for (Element p : paragraphs) {
+            websiteData.append(p.text()).append(" ");
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        // Example usage:
-        String urlToParse = "https://edition.cnn.com/2024/01/29/politics/luttig-conway-supreme-court-trump-insurrection/index.html";
-        String result2 = parser(urlToParse);
-        System.out.println("mm2: " + result2);
+        return websiteData.toString().trim();
     }
 }
 
