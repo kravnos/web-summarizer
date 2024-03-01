367fb44a2e285c580badbed48fc92c14ef02284f
Antonio <abelsito@civiconnect.ca>
2024-03-01 15:47:57 -0500

    Columns changed, comments added

--- src/main/java/com/websummarizer/Web/Summarizer/model/Custom_URLs.java
+++ src/main/java/com/websummarizer/Web/Summarizer/model/Custom_URLs.java
@@ -16,21 +16,28 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Custom_URLs {
 
+    //ID of the custom URL created
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "HID")
+    @Column(name = "CID")
     private Long id;
 
+    //User ID of the person who created it
     @ManyToOne
     @JoinColumn(name = "UID")
     private User user;
 
+    //The custom link created by the user
+    @Setter
+    @Column(name = "Custom_Link", length = 100000)
+    private String customLink;
 
+    //THe destination the custom link goes to
     @Setter
     @Column(name = "Destination_Link", length = 100000)
     private String destinationLink;
 
-
+    //Time which the URL was uploaded
     @Setter
     @Column(name = "upload_time")
     private LocalDateTime uploadTime;
