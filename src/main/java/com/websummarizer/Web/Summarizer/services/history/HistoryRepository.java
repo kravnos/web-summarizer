3334c48fa6bfbfc1c907fd3b74d8639508f8cac7
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-03-05 17:27:02 -0500

    Add comment for method

--- src/main/java/com/websummarizer/Web/Summarizer/services/history/HistoryRepository.java
+++ src/main/java/com/websummarizer/Web/Summarizer/services/history/HistoryRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface HistoryRepository extends CrudRepository<History, Long> {
-
+    //find user's history summarize using short link
     @Query("SELECT h FROM History WHERE h.shortlink = ?1")
     Optional<History> findHistoryByShortlink(String shortLink);
 }
