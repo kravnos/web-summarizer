f9d3faec1074b7ca9f1093815cbfbbb6528ad52e
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-03-10 23:31:13 -0400

    add shortlink file for better url management

---
+++ C4WS21_TEST_V2/COSC-4P02-PROJECT-main/COSC-4P02-PROJECT-main/src/main/java/com/websummarizer/Web/Summarizer/controller/shortlink/Shortlink.java
@@ -0,0 +1,22 @@
+package com.websummarizer.Web.Summarizer.controller.shortlink;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Shortlink {
+    public Shortlink(){
+
+    }
+
+    public String codeShort(String urlOg){
+        int num = Math.abs(urlOg.hashCode());
+        String dic = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String out = "";
+        int len = dic.length();
+        while (num>0){
+            out = dic.charAt(num%len)+out;
+            num = num/len;
+        };
+        return out;
+    }
+}
