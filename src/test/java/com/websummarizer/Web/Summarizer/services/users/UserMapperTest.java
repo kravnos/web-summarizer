1709637c47f34c840bd792dbafd4727cdda5f378
Antonio <abelsito@civiconnect.ca>
2024-03-11 23:42:59 -0400

    Updated test files

--- src/test/java/com/websummarizer/Web/Summarizer/services/users/UserMapperTest.java
+++ src/test/java/com/websummarizer/Web/Summarizer/services/users/UserMapperTest.java
@@ -1,7 +1,20 @@
 package com.websummarizer.Web.Summarizer.services.users;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserMapperTest {
 
+    @Test
+    void mapUserReqAtoToEto() {
+    }
+
+    @Test
+    void mapUserEtoToResAto() {
+    }
+
+    @Test
+    void updateUser() {
+    }
 }
--- src/test/java/com/websummarizer/Web/Summarizer/services/users/UserServiceTest.java
+++ src/test/java/com/websummarizer/Web/Summarizer/services/users/UserServiceTest.java
@@ -1,7 +1,10 @@
 package com.websummarizer.Web.Summarizer.services.users;
 
+import com.websummarizer.Web.Summarizer.services.UserService;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
+
 
 }
