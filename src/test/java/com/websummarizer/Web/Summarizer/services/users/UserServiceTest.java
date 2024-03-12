4e5e387d5d1af77706d239ad6f6b6e5538d1badf
Antonio <abelsito@civiconnect.ca>
2024-03-11 23:45:14 -0400

    Test files both have proper methods to use for testing

--- src/test/java/com/websummarizer/Web/Summarizer/services/users/UserServiceTest.java
+++ src/test/java/com/websummarizer/Web/Summarizer/services/users/UserServiceTest.java
@@ -1,10 +1,28 @@
 package com.websummarizer.Web.Summarizer.services.users;
 
-import com.websummarizer.Web.Summarizer.services.UserService;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
 
+    @Test
+    void addUser() {
+    }
 
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void findAllUser() {
+    }
 }
