0334464e642ab74c084fac5ee765f2dbb15d3d84
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-15 19:41:09 -0400

    Now user implements UserDetails to use spring security

--- src/main/java/com/websummarizer/Web/Summarizer/model/User.java
+++ src/main/java/com/websummarizer/Web/Summarizer/model/User.java
@@ -2,53 +2,72 @@ package com.websummarizer.Web.Summarizer.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
 
 /**
  * Domain object for Affiliate (represents a row in table "users")
  */
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="uid")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "uid")
     private long id;
-
-    @Setter
     @Column(name = "first_name")
     private String first_name;
-
-    @Setter
     @Column(name = "last_name")
     private String last_name;
-
-    @Setter
-    @Column(name = "email" , unique = true)
-    String email;
-
-    @Setter
+    @Column(name = "email", unique = true)
+    private String email;
     @Column(name = "password")
-    String password;
-
-    @Setter
+    private String password;
     @Column(name = "phone_number")
-    String phone_number;
+    private String phone_number;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role_junction",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")}
+    )
+    private Set<Role> authorities;
 
     @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", first_name='" + first_name + '\'' +
-                ", last_name='" + last_name + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", phone_number='" + phone_number + '\'' +
-                '}';
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.first_name + " " + this.last_name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
