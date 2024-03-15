2c60a4dc82db92b544c59c8fc3db02cd39e07c73
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-15 19:18:06 -0400

    added Role.java to define role of different users

---
+++ src/main/java/com/websummarizer/Web/Summarizer/model/Role.java
@@ -0,0 +1,31 @@
+package com.websummarizer.Web.Summarizer.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Integer roleId;
+    private String authority;
+
+    public Role(Integer roleId, String authority) {
+        this.roleId = roleId;
+        this.authority = authority;
+    }
+
+    public Role() {
+        super();
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+}
