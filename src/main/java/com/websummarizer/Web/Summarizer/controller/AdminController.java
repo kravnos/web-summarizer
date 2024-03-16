43730f20572b1bfbefbf215edc394c872f7c03c1
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-15 22:24:01 -0400

    added admin controller

--- src/main/java/com/websummarizer/Web/Summarizer/controller/AdminController.java
+++ src/main/java/com/websummarizer/Web/Summarizer/controller/AdminController.java
@@ -1,11 +1,13 @@
 package com.websummarizer.Web.Summarizer.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
+@CrossOrigin("*")
 public class AdminController {
 
     @GetMapping("/")
