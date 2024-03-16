1ae72ec7c9863e1ddd7d392d48acddd2b372dc7d
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-15 23:13:13 -0400

    added admin Command line runner

--- src/main/java/com/websummarizer/Web/Summarizer/WebSummarizerApplication.java
+++ src/main/java/com/websummarizer/Web/Summarizer/WebSummarizerApplication.java
@@ -1,13 +1,40 @@
 package com.websummarizer.Web.Summarizer;
 
+import com.websummarizer.Web.Summarizer.model.Role;
+import com.websummarizer.Web.Summarizer.model.User;
+import com.websummarizer.Web.Summarizer.repo.RoleRepo;
+import com.websummarizer.Web.Summarizer.repo.UserRepo;
+import com.websummarizer.Web.Summarizer.services.users.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class WebSummarizerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebSummarizerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(RoleRepo roleRepository, UserRepo userRepository, PasswordEncoder passwordEncoder){
+		return args ->{
+			if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("USER"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			User admin = new User(1,"admin","admin","email",passwordEncoder.encode("password"),"phone",roles);
+
+			userRepository.save(admin);
+		};
 	}
 
 }
