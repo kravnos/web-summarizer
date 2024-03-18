27f4a051cc7790ba116e06f55444beac4466ce46
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-18 13:13:33 -0400

    Changed return type to role find by authority

--- src/main/java/com/websummarizer/Web/Summarizer/repo/RoleRepo.java
+++ src/main/java/com/websummarizer/Web/Summarizer/repo/RoleRepo.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface RoleRepo extends JpaRepository<Role,Long> {
-    Optional<Object> findByAuthority(String admin);
+    Optional<Role> findByAuthority(String authority);
 }
