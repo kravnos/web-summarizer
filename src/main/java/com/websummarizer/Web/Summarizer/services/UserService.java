0cde1e20a352e3c3776050241b2f04ac546772f0
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-19 15:21:16 -0400

    no longer needed

--- src/main/java/com/websummarizer/Web/Summarizer/services/UserService.java
+++
@@ -1,8 +0,0 @@
-//package com.websummarizer.Web.Summarizer.services;
-//
-//
-//import com.websummarizer.Web.Summarizer.model.User;
-//
-//public interface UserService{
-//    User createUser(User user);
-//}
