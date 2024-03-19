6cbf0af2f81c95f60879e015afe249831333abe0
Antonio <abelsito@civiconnect.ca>
2024-03-19 15:19:06 -0400

    Added historyservicetest, still needs configuration

---
+++ src/test/java/com/websummarizer/Web/Summarizer/services/history/HistoryServiceTest.java
@@ -0,0 +1,28 @@
+package com.websummarizer.Web.Summarizer.services.history;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HistoryServiceTest {
+
+    @Test
+    void addHistory() {
+    }
+
+    @Test
+    void getHistory() {
+    }
+
+    @Test
+    void updateHistory() {
+    }
+
+    @Test
+    void deleteHistory() {
+    }
+
+    @Test
+    void findAllHistory() {
+    }
+}
