7745d7a137cb97fee3114062c2bef68817c835f8
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-19 15:21:42 -0400

    created service class that generates jwt tokens using JWT encoder

---
+++ src/main/java/com/websummarizer/Web/Summarizer/services/TokenService.java
@@ -0,0 +1,36 @@
+package com.websummarizer.Web.Summarizer.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@Service
+public class TokenService {
+    @Autowired
+    private JwtEncoder jwtEncoder;
+    @Autowired
+    private JwtDecoder jwtDecoder;
+
+    public String generateJwt(Authentication authentication){
+        Instant now = Instant.now();
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .subject(authentication.getName())
+                .claim("role",scope)
+                .build();
+        return jwtEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+    }
+}
