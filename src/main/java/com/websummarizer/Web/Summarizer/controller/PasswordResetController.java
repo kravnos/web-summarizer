5ee9ac676a74578334a0cf12a451aff54a811b58
Ralph Terte <109493278+Jaserii@users.noreply.github.com>
2024-04-05 15:10:24 -0400

    Move password reset mappings

--- src/main/java/com/websummarizer/Web/Summarizer/controller/PasswordResetController.java
+++ src/main/java/com/websummarizer/Web/Summarizer/controller/PasswordResetController.java
@@ -13,6 +13,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,81 @@ public class PasswordResetController {
     private JavaMailSender emailSender;
     private static final Logger logger = Logger.getLogger("PasswordResetController");
 
+    /**
+     * Endpoint for send authentication code.
+     *
+     * @return The name of the view to render.
+     */
+    @PostMapping("/user/code")
+    public String code(
+            @RequestParam(value = "login_email") String email,
+            Model model
+    ) {
+        model.addAttribute("email", email);
+
+        return "user/code";
+    }
+
+    /**
+     * Endpoint for send authentication code.
+     *
+     * @return The name of the view to render.
+     */
+    @PostMapping("/user/send")
+    public String send(
+            @RequestParam(value = "code_email") String email,
+            Model model
+    ) {
+        boolean isValidEmail = true;        /* TODO: validate email exists in the DB */
+
+        model.addAttribute("email", email);
+
+        if (isValidEmail) {
+            model.addAttribute("isValid", true);
+            model.addAttribute("html", "<span class=\"bi bi-check-circle-fill\"></span>");
+            model.addAttribute("message", "Authentication Code sent to '" + email + "'. Please check your inbox.");
+
+            return "user/reset";
+        } else {
+            model.addAttribute("isValid", false);
+            model.addAttribute("html", "<span class=\"bi bi-exclamation-triangle-fill\"></span>");
+            model.addAttribute("message", "No account found for '" + email + "'. Please try again.");
+
+            return "user/code";
+        }
+    }
+
+    /**
+     * Endpoint for reset password.
+     *
+     * @return The name of the view to render.
+     */
+    @PostMapping("/user/reset")
+    public String reset(
+            @RequestParam(value = "reset_email") String email,
+            @RequestParam(value = "reset_password") String password,
+            @RequestParam(value = "reset_code") String code,
+            Model model
+    ) {
+        boolean isValidEmail = true;    /* TODO: validate email exists in the DB */
+        boolean isValidCode = true;     /* TODO: validate auth with code sent in email */
+
+        if ((isValidEmail) && (isValidCode)) {
+            model.addAttribute("email", email);
+            model.addAttribute("isValid", true);
+            model.addAttribute("html", "<span class=\"bi bi-check-circle-fill\"></span>");
+            model.addAttribute("message", "Password for account '" + email + "' has been reset. Please login.");
+
+            return "user/login";
+        } else {
+            model.addAttribute("isValid", false);
+            model.addAttribute("html", "<span class=\"bi bi-exclamation-triangle-fill\"></span>");
+            model.addAttribute("message", "Authentication error for '" + email + "'. Please try again.");
+
+            return "user/reset";
+        }
+    }
+    
     /**
      * Runs when a user selects "Forgot Password" and submits an email address
      *
