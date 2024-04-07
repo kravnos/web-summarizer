6d751ce1e54f53af60e0d92bea049458105450b7
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-04-07 18:16:40 -0400

    created service class for oauth 2 clients

--- src/main/java/com/websummarizer/Web/Summarizer/services/UserOAuth2Service.java
+++ src/main/java/com/websummarizer/Web/Summarizer/services/UserOAuth2Service.java
@@ -5,7 +5,6 @@ import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserServ
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +12,8 @@ public class UserOAuth2Service extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        return new UserOAuth2(super.loadUser(userRequest));
+        OAuth2User userOAuth2 = super.loadUser(userRequest);
+        return new UserOAuth2(userOAuth2);
     }
 
 }
