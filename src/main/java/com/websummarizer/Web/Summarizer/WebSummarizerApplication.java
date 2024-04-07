20126bb5f2a2cb8b3bbc8a530880b44c6beffa6d
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-04-07 18:17:31 -0400

    changed admin provider to local and phone number to null

--- src/main/java/com/websummarizer/Web/Summarizer/WebSummarizerApplication.java
+++ src/main/java/com/websummarizer/Web/Summarizer/WebSummarizerApplication.java
@@ -1,10 +1,10 @@
 package com.websummarizer.Web.Summarizer;
 
+import com.websummarizer.Web.Summarizer.model.Provider;
 import com.websummarizer.Web.Summarizer.model.Role;
 import com.websummarizer.Web.Summarizer.model.User;
 import com.websummarizer.Web.Summarizer.repo.RoleRepo;
 import com.websummarizer.Web.Summarizer.repo.UserRepo;
-import com.websummarizer.Web.Summarizer.services.users.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -31,7 +31,7 @@ public class WebSummarizerApplication {
 			Set<Role> roles = new HashSet<>();
 			roles.add(adminRole);
 
-			User admin = new User(1,"ADMIN","ADMIN","admin@email.com",passwordEncoder.encode("password"),"14444444444", roles);
+			User admin = new User(1,"ADMIN","ADMIN","admin@email.com",passwordEncoder.encode("password"),"null", roles, Provider.LOCAL);
 			userRepository.save(admin);
 		};
 	}
