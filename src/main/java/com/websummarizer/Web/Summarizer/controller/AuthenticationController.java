0299ac0178809c8d9a0279306de73f594cdc6188
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-04-07 18:22:13 -0400

    code clean up

--- src/main/java/com/websummarizer/Web/Summarizer/controller/AuthenticationController.java
+++ src/main/java/com/websummarizer/Web/Summarizer/controller/AuthenticationController.java
@@ -7,7 +7,6 @@ import com.websummarizer.Web.Summarizer.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
