34e556565e96c935d3abfab84ac3446bddc1679d
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-04-10 22:54:44 -0400

    get user email

--- src/main/java/com/websummarizer/Web/Summarizer/services/users/UserRepository.java
+++ src/main/java/com/websummarizer/Web/Summarizer/services/users/UserRepository.java
@@ -1,9 +1,14 @@
 package com.websummarizer.Web.Summarizer.services.users;
 
 import com.websummarizer.Web.Summarizer.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> findByEmail(String email); //SELECT * FROM users WHERE email = ...
 }
