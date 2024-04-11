8486eeb08827a88c9a05ecbae13f6dde70efab8f
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-04-10 22:53:49 -0400

    add bitly to shortlink. Move checkpassword to Shortlink

--- src/main/java/com/websummarizer/Web/Summarizer/controller/shortlink/Shortlink.java
+++ src/main/java/com/websummarizer/Web/Summarizer/controller/shortlink/Shortlink.java
@@ -1,12 +1,45 @@
 package com.websummarizer.Web.Summarizer.controller.shortlink;
 
+import com.websummarizer.Web.Summarizer.controller.BitLyController;
+import com.websummarizer.Web.Summarizer.controller.HistoryController;
+import com.websummarizer.Web.Summarizer.controller.history.HistoryReqAto;
+import com.websummarizer.Web.Summarizer.services.users.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 @RestController
 public class Shortlink {
-    public Shortlink(){
+    @Autowired
+    BitLyController bitLyController;
+
+    @Autowired
+    HistoryController historyController;
+
+    @Autowired
+    UserService userService;
+
+    public String Shortlink(String url, String output, HttpSession session){
+
+        String email = (String) session.getAttribute("username");
+        Long id = userService.getFoundId(email);
+
+        // Generate a short code for the given URL
+        String ShortlinkCode = codeShort(url);
+        // Create a new history request object with the generated short code
+        HistoryReqAto historyReqAto = new HistoryReqAto(id, output, ShortlinkCode, LocalDateTime.now());
+        // Add the history request to the database and get the response
+        historyController.addHistory(historyReqAto);
+
+        String shortBitly = bitLyController.processBitly("http://***.***.*.***:8080/"+ShortlinkCode); //need domain to make this function work,  else this is just the pc ip address to test
+
+        return shortBitly;
 
     }
 
@@ -24,4 +57,25 @@ public class Shortlink {
         };
         return out;
     }
+
+    //unfinished checkpassword function
+    public boolean checkPassword(String password){
+        String pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+
+        // Compile the pattern
+        Pattern regex = Pattern.compile(pattern);
+
+        // Create a Matcher object
+        Matcher matcher = regex.matcher(password);
+        return matcher.matches();
+    }
+
+//    //check password
+//        if(!shortlink.checkPassword(password)) {
+//        model.addAttribute("isValid", false);
+//        model.addAttribute("html", "<span class=\"bi bi-exclamation-triangle-fill\"></span>");
+//        model.addAttribute("message", "Password must contain at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character");
+//
+//        return "user/register";
+//    }
 }
