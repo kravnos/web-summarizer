fbc72bbac6fb62ce482681fbbee94ecf1b2649d1
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-04-22 15:46:42 -0400

    commented unwanted classes

--- src/main/java/com/websummarizer/Web/Summarizer/controller/HistoryController.java
+++ src/main/java/com/websummarizer/Web/Summarizer/controller/HistoryController.java
@@ -1,64 +1,64 @@
-package com.websummarizer.Web.Summarizer.controller;
-
-import com.websummarizer.Web.Summarizer.model.history.HistoryReqAto;
-import com.websummarizer.Web.Summarizer.model.history.HistoryResAto;
-import com.websummarizer.Web.Summarizer.model.history.HistoriesResAto;
-import com.websummarizer.Web.Summarizer.services.history.HistoryService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/v1/history")
-@Slf4j
-public class HistoryController {
-    @Autowired
-    private HistoryService historyService;
-
-    // Add a new history record
-    @PostMapping
-    public ResponseEntity<HistoryResAto> addHistory(@RequestBody HistoryReqAto historyReqAto) {
-        log.info("Add history request received : " + historyReqAto);
-        var historyResAto = historyService.addHistory(historyReqAto);
-        log.info("Add history request processed : " + historyReqAto);
-        return ResponseEntity.ok(historyResAto);
-    }
-
-    // Delete a history record by ID
-    @DeleteMapping("/{id}")
-    public ResponseEntity deleteHistory(@PathVariable Long id) {
-        log.info("Delete history request received : " + id);
-        historyService.deleteHistory(id);
-        log.info("Delete history request processed : " + id);
-        return ResponseEntity.ok().build();
-    }
-
-    // Get a history record by ID
-    @GetMapping("/{id}")
-    public ResponseEntity<HistoryResAto> getHistory(@PathVariable Long id) {
-        log.info("Get history request received : " + id);
-        var historyResAto = historyService.getHistory(id);
-        log.info("Get history request processed : " + historyResAto);
-        return ResponseEntity.ok(historyResAto);
-    }
-
-    // Get all history records
-    @GetMapping()
-    public ResponseEntity<HistoriesResAto> findAllHistory() {
-        log.info("Find all history request received");
-        var allHistories = historyService.findAllHistory();
-        log.info("Find all history request processed. Total count : " + allHistories.getTotalCount());
-        return ResponseEntity.ok(allHistories);
-    }
-
-    // Update a history record by ID
-    @PutMapping("/{id}")
-    public ResponseEntity<HistoryResAto> updateHistory(@PathVariable Long id,
-                                                       @RequestBody HistoryReqAto historyReqAto) {
-        log.info(String.format("Update history request received [id=%s, req=%s]", id, historyReqAto));
-        var historyResAto = historyService.updateHistory(id, historyReqAto);
-        log.info("Update history request processed : " + historyResAto);
-        return ResponseEntity.ok(historyResAto);
-    }
-}
+//package com.websummarizer.Web.Summarizer.controller;
+//
+//import com.websummarizer.Web.Summarizer.model.history.HistoryReqAto;
+//import com.websummarizer.Web.Summarizer.model.history.HistoryResAto;
+//import com.websummarizer.Web.Summarizer.model.history.HistoriesResAto;
+//import com.websummarizer.Web.Summarizer.services.history.HistoryService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/v1/history")
+//@Slf4j
+//public class HistoryController {
+//    @Autowired
+//    private HistoryService historyService;
+//
+//    // Add a new history record
+//    @PostMapping
+//    public ResponseEntity<HistoryResAto> addHistory(@RequestBody HistoryReqAto historyReqAto) {
+//        log.info("Add history request received : " + historyReqAto);
+//        var historyResAto = historyService.addHistory(historyReqAto);
+//        log.info("Add history request processed : " + historyReqAto);
+//        return ResponseEntity.ok(historyResAto);
+//    }
+//
+//    // Delete a history record by ID
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity deleteHistory(@PathVariable Long id) {
+//        log.info("Delete history request received : " + id);
+//        historyService.deleteHistory(id);
+//        log.info("Delete history request processed : " + id);
+//        return ResponseEntity.ok().build();
+//    }
+//
+//    // Get a history record by ID
+//    @GetMapping("/{id}")
+//    public ResponseEntity<HistoryResAto> getHistory(@PathVariable Long id) {
+//        log.info("Get history request received : " + id);
+//        var historyResAto = historyService.getHistory(id);
+//        log.info("Get history request processed : " + historyResAto);
+//        return ResponseEntity.ok(historyResAto);
+//    }
+//
+//    // Get all history records
+//    @GetMapping()
+//    public ResponseEntity<HistoriesResAto> findAllHistory() {
+//        log.info("Find all history request received");
+//        var allHistories = historyService.findAllHistory();
+//        log.info("Find all history request processed. Total count : " + allHistories.getTotalCount());
+//        return ResponseEntity.ok(allHistories);
+//    }
+//
+//    // Update a history record by ID
+//    @PutMapping("/{id}")
+//    public ResponseEntity<HistoryResAto> updateHistory(@PathVariable Long id,
+//                                                       @RequestBody HistoryReqAto historyReqAto) {
+//        log.info(String.format("Update history request received [id=%s, req=%s]", id, historyReqAto));
+//        var historyResAto = historyService.updateHistory(id, historyReqAto);
+//        log.info("Update history request processed : " + historyResAto);
+//        return ResponseEntity.ok(historyResAto);
+//    }
+//}
--- src/main/java/com/websummarizer/Web/Summarizer/model/user/UsersResAto.java
+++ src/main/java/com/websummarizer/Web/Summarizer/model/user/UsersResAto.java
@@ -1,21 +1,21 @@
-package com.websummarizer.Web.Summarizer.model.user;
-
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
-
-import java.util.List;
-
-@Builder
-@Getter
-@ToString(onlyExplicitlyIncluded = true)
-public class UsersResAto {
-    private List<UserResAto> users;
-
-    public int getTotalCount() {
-        if (users == null) {
-            return 0;
-        }
-        return users.size();
-    }
-}
+//package com.websummarizer.Web.Summarizer.model.user;
+//
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.ToString;
+//
+//import java.util.List;
+//
+//@Builder
+//@Getter
+//@ToString(onlyExplicitlyIncluded = true)
+//public class UsersResAto {
+//    private List<UserResAto> users;
+//
+//    public int getTotalCount() {
+//        if (users == null) {
+//            return 0;
+//        }
+//        return users.size();
+//    }
+//}
